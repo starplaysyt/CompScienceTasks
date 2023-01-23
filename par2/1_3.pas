755d4f926bdf7ad28c1cc11f08d70c10e2ce051a
SPlaysYT <horaudmitry@gmail.com>
2023-01-23 21:41:51 +0300

    rev 1.1.2

---
+++ par2/1_3.pas
@@ -0,0 +1,13 @@
+﻿uses System;
+var n, i: integer; 
+var sum: real;
+begin
+  Write('Insert n: ');
+  Read(n);
+  sum:= 0;
+  for i:= 1 to n do
+  begin
+    sum:= sum + 1/Math.Pow(i, 2);
+  end;
+  Write(sum);
+end.
---
+++ par2/1_4.pas
@@ -0,0 +1,23 @@
+﻿uses System;
+var subStr1, subStr2:string;
+var str: string;
+begin
+  Write('Insert string: ');
+  Read(str);
+  str:= str.ToLower();
+  str:= str.Replace(' ', '');
+  if str.Length mod 2 <> 0 then
+    begin
+      subStr1 := str.Substring(0, Convert.ToInt32(str.Length/2)-1);
+      subStr2 := string.Join('', str.Substring(Convert.ToInt32(str.Length/2), Convert.ToInt32(str.Length/2)-1).ToCharArray().Reverse());
+    end
+  else 
+    begin
+      subStr1 := str.Substring(0, Convert.ToInt32(str.Length/2));
+      subStr2 := string.Join('', str.Substring(Convert.ToInt32(str.Length/2), Convert.ToInt32(str.Length/2)).ToCharArray().Reverse());
+    end;
+  if subStr1.Contains(subStr2) then
+    Write('This shit is polyndrom')
+  else
+    Write('This shit isnt polyndrom');
+end.
